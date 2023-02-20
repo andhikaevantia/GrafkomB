97567b3e0bf75c9f978f9e3e8b6261eb5641c345
Justinus <justin@petra.ac.id>
2023-02-20 16:35:24 +0700

    Week2 Final

--- src/main/java/Engine/Object2d.java
+++ src/main/java/Engine/Object2d.java
@@ -1,6 +1,7 @@
 package Engine;
 
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 import java.util.List;
 
@@ -15,12 +16,31 @@ public class Object2d extends ShaderProgram{
     List<Vector3f> vertices;
     int vao;
     int vbo;
+    Vector4f color;
+    UniformsMap uniformsMap;
+
+    List<Vector3f> verticesColor;
+    int vboColor;
+
     public Object2d(List<ShaderModuleData> shaderModuleDataList,
-                    List<Vector3f> vertices) {
+                    List<Vector3f> vertices,
+                    Vector4f color) {
         super(shaderModuleDataList);
         this.vertices = vertices;
         setupVAOVBO();
+        this.color = color;
+        uniformsMap = new UniformsMap(getProgramId());
+        uniformsMap.createUniform(
+                "uni_color");
 
+    }
+    public Object2d(List<ShaderModuleData> shaderModuleDataList,
+                    List<Vector3f> vertices,
+                    List<Vector3f> verticesColor) {
+        super(shaderModuleDataList);
+        this.vertices = vertices;
+        this.verticesColor = verticesColor;
+        setupVAOVBOWithVerticesColor();
     }
     public void setupVAOVBO(){
         //set vao
@@ -34,12 +54,50 @@ public class Object2d extends ShaderProgram{
                 Utils.listoFloat(vertices),
                 GL_STATIC_DRAW);
     }
+    public void setupVAOVBOWithVerticesColor(){
+        //set vao
+        vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+        //set vbo
+        vbo = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        //mengirim vertices ke shader
+        glBufferData(GL_ARRAY_BUFFER,
+                Utils.listoFloat(vertices),
+                GL_STATIC_DRAW);
+        //set vboColor
+        vboColor = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboColor);
+        //mengirim vbocolor ke shader
+        glBufferData(GL_ARRAY_BUFFER,
+                Utils.listoFloat(verticesColor),
+                GL_STATIC_DRAW);
+    }
     public void drawSetup(){
+        bind();
+        uniformsMap.setUniform(
+                "uni_color", color);
+        // Bind VBO
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexAttribPointer(0,
+                3, GL_FLOAT,
+                false,
+                0, 0);
+    }
+    public void drawSetupWithVerticesColor(){
         bind();
         // Bind VBO
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glVertexAttribPointer(0,
+                3, GL_FLOAT,
+                false,
+                0, 0);
+        // Bind VBOColor
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, vboColor);
+        glVertexAttribPointer(1,
                 3, GL_FLOAT,
                 false,
                 0, 0);
@@ -55,7 +113,21 @@ public class Object2d extends ShaderProgram{
         //GL_LINES
         //GL_POINTS
         //GL_TRIANGLE_FAN
-        glDrawArrays(GL_LINE_LOOP, 0,
+        glDrawArrays(GL_TRIANGLES, 0,
+                vertices.size());
+    }
+    public void drawWithVerticesColor(){
+        drawSetupWithVerticesColor();
+        // Draw the vertices
+        glLineWidth(10);
+        glPointSize(10);
+        //GL_TRIANGLES
+        //GL_LINE_LOOP
+        //GL_LINE_STRIP
+        //GL_LINES
+        //GL_POINTS
+        //GL_TRIANGLE_FAN
+        glDrawArrays(GL_TRIANGLES, 0,
                 vertices.size());
     }
 }
